18a157510dcb823319267a69c1c038ba76ed6103
yaoys <312573909@qq.com>
2023-05-16 17:44:15 +0800

    前端代码

---
+++ saas-hrm-vue/src/api/constant/attendance.js
@@ -0,0 +1,47 @@
+// 员工端
+export default {
+  // 假期类型
+  holidayType: [
+    {
+      id: '1',
+      value: '正常'
+    },
+    {
+      id: '2',
+      value: '旷工'
+    },
+    {
+      id: '3',
+      value: '迟到'
+    },
+    {
+      id: '4',
+      value: '早退'
+    },
+    {
+      id: '6',
+      value: '出差'
+    },
+    {
+      id: '8',
+      value: '事假'
+    },
+    {
+      id: '9',
+      value: '病假'
+    },
+    {
+      id: '12',
+      value: '产假'
+    },
+    {
+      id: '17',
+      value: '调休'
+    },
+    {
+      id: '22',
+      value: '补签'
+    },
+    
+  ]
+}
